9a61d32a546e1c5df0b3964585d2899f78597bdb
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2025-10-24 16:09:17 +0530

    latest

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -86,16 +86,15 @@ async def give_filter(client, message):
             await message.reply_text(
                 f"🚨 **Hello {user.mention}! - Warning {count}/3**⚠️\n\n"
                 f"❌ **Reason:** {reason_text}\n\n"
-                f"📌 **Note:** Spam messages are not allowed. Please follow the group rules to avoid further ban."
+                f"📌 **Note:**\nSpam messages are not allowed.\nPlease follow the group rules to avoid further ban."
             )
         else:
             try:
                 await client.ban_chat_member(chat_id, user_id)
                 await message.reply_text(
-                    f"🚨⚠️ **Alert! {user.mention}** ⚠️🚨\n\n"
-                    f"❌ You have reached already **3 warnings**.\n"
-                    f"🚫 As a result, you are now **banned** from the group.\n\n"
-                    f"📌 For further support, please contact the group admins."
+                    f"🚨 **Ban Alert - {user.mention}!** 🚨\n\n"
+                    f"⚠️ You have reached already **3 Warnings**.\n\n"
+                    f"🚫 You are now **Banned!** from the group."
                 )
                 user_warnings[chat_id].pop(user_id, None)
                 if LOG_CHANNEL:
