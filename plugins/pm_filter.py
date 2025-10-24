fea433d7b8269d5e276a37dcd2a4cb4f3cfedaf8
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2025-10-24 15:55:47 +0530

    latest

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -84,9 +84,9 @@ async def give_filter(client, message):
             reason_text = ", ".join(reasons)
             
             await message.reply_text(
-                f"**🚨 {user.mention} - Warning {count}/3** ⚠️\n" 
-                f"**❌ Reason:** {reason_text}\n\n" 
-                f"**📌 Note:** Spam Messages are not allowed, Please follow the group rules."
+                f"🚨 **Hello {user.mention}! - Warning {count}/3**⚠️\n\n"
+                f"❌ **Reason:** {reason_text}\n\n"
+                f"📌 **Note:** Spam messages are not allowed. Please follow the group rules to avoid further ban."
             )
         else:
             try:
